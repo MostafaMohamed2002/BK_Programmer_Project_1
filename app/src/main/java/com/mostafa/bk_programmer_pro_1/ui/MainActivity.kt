f56806f8fabfafdd431dfddd4c74c6dcbf857aed
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-01-17 09:12:15 +0200

    use the csvParser , view data in logcat

--- app/src/main/java/com/mostafa/bk_programmer_pro_1/ui/MainActivity.kt
+++ app/src/main/java/com/mostafa/bk_programmer_pro_1/ui/MainActivity.kt
@@ -2,28 +2,28 @@ package com.mostafa.bk_programmer_pro_1.ui
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.mostafa.bk_programmer_pro_1.R
 import com.mostafa.bk_programmer_pro_1.databinding.ActivityMainBinding
+import com.mostafa.bk_programmer_pro_1.util.CsvParser
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
-    var _binding : ActivityMainBinding? = null
+    var _binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding= ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding?.root)
         openFile()
     }
-    private fun openFile(){
+
+    private fun openFile() {
         val inputStream = assets.open("worldcup.csv")
         val buffer = BufferedReader(InputStreamReader(inputStream))
-        buffer. forEachLine {
-            Log.v("DATA",it)
+        val csvParser = CsvParser()
+        buffer.forEachLine {
+            val currentMatch = csvParser.parse(it)
+            Log.v("DATA", currentMatch.toString())
         }
     }
 }
