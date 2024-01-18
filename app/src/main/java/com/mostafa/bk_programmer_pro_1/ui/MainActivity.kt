5074de1e6cd7f7458a02772eea1fc73382537e07
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-01-18 02:21:07 +0200

    + bindmatches() , callbacks()

--- app/src/main/java/com/mostafa/bk_programmer_pro_1/ui/MainActivity.kt
+++ app/src/main/java/com/mostafa/bk_programmer_pro_1/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mostafa.bk_programmer_pro_1.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.mostafa.bk_programmer_pro_1.data.DataManager
+import com.mostafa.bk_programmer_pro_1.data.domain.Match
 import com.mostafa.bk_programmer_pro_1.databinding.ActivityMainBinding
 import com.mostafa.bk_programmer_pro_1.util.CsvParser
 import java.io.BufferedReader
@@ -14,16 +16,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding?.root)
-        openFile()
+        parseFile()
+        applyCallBacks()
     }
 
-    private fun openFile() {
+    private fun parseFile() {
         val inputStream = assets.open("worldcup.csv")
         val buffer = BufferedReader(InputStreamReader(inputStream))
         val csvParser = CsvParser()
         buffer.forEachLine {
-            val currentMatch = csvParser.parse(it)
-            Log.v("DATA", currentMatch.toString())
+            val currentMatch: Match = csvParser.parse(it)
+            DataManager.addMatch(currentMatch)
+        }
+        bindMatch(DataManager.getCurrentMatch())
+    }
+
+    fun applyCallBacks() {
+        _binding?.apply {
+            nextButton.setOnClickListener {
+                val match = DataManager.getNextMatch()
+                bindMatch(match)
+                Log.v("MatchID", DataManager.matchIndex.toString())
+            }
+            prevButton.setOnClickListener {
+                var match = DataManager.getPrevMatch()
+                bindMatch(match)
+                Log.v("MatchID", DataManager.matchIndex.toString())
+            }
         }
     }
+
+    private fun bindMatch(match: Match) {
+        _binding?.apply {
+            yearTextView.text = match.year.toString()
+            stadiumTextView.text = match.stadium.toString()
+            homeTeamNameTextView.text = match.homeTeamName.toString()
+            homeTeamGoalsTextView.text = match.homeTeamGoals.toString()
+            awayTeamGoalsTextView.text = match.awayTeamGoals.toString()
+            awayTeamNameTextView.text = match.awayTeamName
+        }
+    }
+
 }
