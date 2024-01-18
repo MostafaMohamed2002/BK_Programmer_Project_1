e9ba5f7de39147fe10ad5c648e21bd2a2c27f722
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-01-18 02:20:38 +0200

    complete implementation

--- app/src/main/java/com/mostafa/bk_programmer_pro_1/data/DataManager.kt
+++ app/src/main/java/com/mostafa/bk_programmer_pro_1/data/DataManager.kt
@@ -1,4 +1,32 @@
 package com.mostafa.bk_programmer_pro_1.data
 
+import com.mostafa.bk_programmer_pro_1.data.domain.Match
+
 object DataManager {
+    private val matchesList = mutableListOf<Match>()
+    var matchIndex = 0
+    fun addMatch(match: Match) {
+        matchesList.add(match)
+    }
+
+    fun getCurrentMatch(): Match {
+        return matchesList[matchIndex]
+    }
+
+    fun getNextMatch(): Match {
+        //handle end of array error
+        if (matchIndex + 1 == matchesList.size - 1) {
+            matchIndex = 0
+        } else
+            matchIndex++
+        return matchesList[matchIndex]
+    }
+
+    fun getPrevMatch(): Match {
+        if (matchIndex - 1 == 0) {
+            matchIndex = matchesList.size - 1
+        } else
+            matchIndex--
+        return matchesList[matchIndex]
+    }
 }
