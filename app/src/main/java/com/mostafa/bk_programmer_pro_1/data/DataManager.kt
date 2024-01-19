ede7d69766464b97c1c68d6c06991c5c075d7b23
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-01-19 10:41:31 +0200

    add getMatchesCount() to data manager

--- app/src/main/java/com/mostafa/bk_programmer_pro_1/data/DataManager.kt
+++ app/src/main/java/com/mostafa/bk_programmer_pro_1/data/DataManager.kt
@@ -4,6 +4,10 @@ import com.mostafa.bk_programmer_pro_1.data.domain.Match
 
 object DataManager {
     private val matchesList = mutableListOf<Match>()
+    val getMatches: List<Match>
+        get() = matchesList
+    val getMatchesCount: Int
+        get() = matchesList.size
     var matchIndex = 0
     fun addMatch(match: Match) {
         matchesList.add(match)
@@ -23,7 +27,7 @@ object DataManager {
     }
 
     fun getPrevMatch(): Match {
-        if (matchIndex - 1 == 0) {
+        if (matchIndex - 1 == 0 || matchIndex == 0) {
             matchIndex = matchesList.size - 1
         } else
             matchIndex--
